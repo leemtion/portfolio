c7d9241c383befdd8cabda9631e175b3564612b3
leemtion <leemtion@naver.com>
2022-06-10 15:22:19 +0900

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+// make navbar transparent when it is the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=>{
+    if(window.scrollY> navbarHeight){
+        navbar.classList.add('navbar--dark');
+    } else{
+        navbar.classList.remove('navbar--dark');
+    }
+});

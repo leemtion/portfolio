c01a5572206e96f1189dc5ad75067c31f95d3ad3
leemtion <leemtion@naver.com>
2022-06-12 14:32:12 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -39,7 +39,20 @@ document.addEventListener('scroll', ()=>{
 });
 
 
+// show "arrow up" button when scrolling down
+const arrowUp= document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=>{
+    if(window.scrollY> homeHeight/2){
+        arrowUp.classList.add('visible');
+    } else{
+        arrowUp.classList.remove('visible');
+    }
+})
 
+// handle click on the "arrow up" button
+arrowUp.addEventListener('click', ()=>{
+    scrollIntoView('#home')
+});
 
 
 

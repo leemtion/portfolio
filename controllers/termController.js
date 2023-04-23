3f21ee7320b007d1414a1abcc248c7f2e0738379
leemtion <leemtion@naver.com>
2023-04-23 20:44:57 +0900

    icon_changed

--- controllers/termController.js
+++ controllers/termController.js
@@ -1,6 +1,6 @@
 export const TermsofService= async(req,res) => {
     console.log("this is TermsofService")
-    return res.render("index.html")     
+    return res.render("hey.html")     
 }
 export const Privacy= async(req,res) => {
     console.log("this is Privacy")
